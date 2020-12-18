e853fd65902556a4791b4e749f367d8d048e2654
rahulomprasad <61745278+rahulomprasad@users.noreply.github.com>
2020-12-18 20:24:57 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -9,19 +9,20 @@ class Author(models.Model):
     name=models.CharField(max_length=50)
     email=models.EmailField()
     bio=models.TextField()
-    #def __str__(self):
-    #return self.name
-class Category(models.Model):
+    def __str__(self):
+    return self.name
+"""class Category(models.Model):
     Category=models.CharField(max_length=50)
     description=models.TextField()
     #def __str__(self):
     #return self.category
+    """
 class Article(models.Model):
     title=models.CharField(max_length=250)
     image=models.ImageField(upload_to="images/")
     content=models.TextField(max_length=6000)
     author=models.ForeignKey(Author, on_delete=models.CASCADE)
-    Category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    #Category=models.ForeignKey(Category,on_delete=models.CASCADE)
     #def __str__(self):
     #return self.title
 
